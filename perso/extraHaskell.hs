f1693a9a596cfbd29c0abac441137810b077a498
Alpacow <fvpereira@inf.ufsm.br>
2019-04-22 18:01:23 -0300

    Terminado ex 1 da atividade personalizada

--- perso/extraHaskell.hs
+++ perso/extraHaskell.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 -- Problema 1: Primo Rápido
 -- Descrição: https://www.urionlinejudge.com.br/judge/pt/problems/view/1221
 
@@ -9,14 +11,24 @@ countGraos i nrCasas graos =
         then graos + countGraos (i + 1) nrCasas graos * 2
     else graos
 
-trigoNoTabuleiro :: Int -> IO()
-trigoNoTabuleiro nrCasas = putStrLn (show ( (countGraos 1 nrCasas 1) `quot` 12 `quot` 1000 ) ++ " Kg")
+getKg :: Int -> String
+getKg nrCasas = show ( (countGraos 1 nrCasas 1) `quot` 12 `quot` 1000 ) ++ " Kg"
+
+trigoNoTabuleiro :: IO()
+trigoNoTabuleiro = do 
+    line1 <- getLine
+    let nCases = (read line1 :: Int)
+    if(nCases >= 1 && nCases <= 100)
+        then forM_ [1..nCases] $ \_ -> do
+                line2 <- getLine
+                let n = read line2 :: Int
+                if(n >= 1 && n <= 64)
+                    then putStrLn $ getKg n
+                    else putStrLn ""
+        else putStrLn ""
 
 -- Problema 3: Angry Ducks
 -- Descrição: https://www.urionlinejudge.com.br/judge/pt/problems/view/1163
-
--- Problema 4: Blobs
--- Descrição: https://www.urionlinejudge.com.br/judge/pt/problems/view/1170
 
 -- Problema 10: Integral
 -- Descrição: https://www.urionlinejudge.com.br/judge/pt/problems/view/1230
