b311569dbba362b654a5b98c0afb929436ba31c4
Francielle Vasconcellos Pereira <fvpereira@lenovo01.inf.ufsm.br>
2019-04-29 16:30:33 -0300

    T3 finalizado

--- t3/t3.pl
+++ t3/t3.pl
@@ -59,20 +59,24 @@ potN0(N, [H|T]) :-
 
 % Ex 10
 zipmult([], [], []).
-zipmult([H|T], [C|R], [Head, Tail]) :-
+zipmult([H|T], [C|R], [Head|Tail]) :-
   Head is H * C,
-  zipmult(T, R, Tail) .
+  zipmult(T, R, Tail), ! .
 
-% Ex 9
-count(0, 0).
-count(I, N) :-
-  I < N,
-  R is I + 1,
-  count(R, N) .
-
-potencias(0, []).
-potencias(N, [H|T]) :-
-  N > 0,
-  I is N - 1,
+% Ex 11
+potenciasAux(I, N, []) :- N =< I .
+potenciasAux(I, N, [H|T]) :-
   H is 2 ^ I,
-  potencias(I, T) .
+  X is I + 1,
+  potenciasAux(X, N, T), !.
+
+potencias(N, L) :-
+  I is 0,
+  potenciasAux(I, N, L).
+
+% Ex 12
+cedulas(_,[],[]).
+cedulas(V, [H|T], [C|R]) :-
+  C is V//H,
+  V1 is mod(V, H),
+  cedulas(V1, T , R), !.
